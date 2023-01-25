6b12607ffeb328ad619fce7c9dd5952d84e86f77
NajhelyBS <s5anbandasanchez@gmail.com>
2023-01-25 09:52:38 -0500

    working in guards

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
-
+import { HeaderComponent } from './header/header.component';
+import { Router, RouterLink } from '@angular/router';
+import { RouterLinkActive } from '@angular/router';
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterLink,
+    RouterLinkActive,
   ],
   exports: [
     HomeComponent,
+    HeaderComponent,
   ]
 })
 export class PagesModule { }
